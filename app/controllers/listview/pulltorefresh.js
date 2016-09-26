9c0444865de99983d745468ef5fdc747fb35853e
InGrowth <gimdongwoo@gmail.com>
2016-09-26 15:02:01 +0900

    listview pull to refresh

--- app/controllers/listview/pulltorefresh.js
+++ app/controllers/listview/pulltorefresh.js
@@ -26,6 +26,57 @@ $.init = function() {
 	}
 };
 
+// fetch from parse
+// STUDY : http://parseplatform.github.io/docs/js/guide/#queries
+// {e} is pulltorefresh event
+CTX.fetchGameScore = function(e) {
+	var GameScore = Parse.Object.extend("GameScore");
+	var query = new Parse.Query(GameScore);
+	// query.equalTo("playerName", "Dan Stemkoski");
+	query.find({
+	  success: function(results) {
+			APP.log("debug", "Successfully retrieved " + results.length + " scores.");
+	    // Do something with the returned Parse.Object values
+	    CTX.drawGameScore(results);
+
+			if (e) e.hide();
+	  },
+	  error: function(error) {
+			APP.log("error", "Error: " + error.code + " " + error.message);
+
+			if (e) e.hide();
+	  }
+	});
+};
+
+// drawGameScore
+CTX.drawGameScore = function(GameScoreCollection) {
+	// using undersocre.js _.map function
+	// STUDY : http://underscorejs.org/
+	var GameScoreRows = _.map(GameScoreCollection, function (GameScoreModel){
+    return CTX.createGameSCoreRow(GameScoreModel);
+  });
+  //값넣기.
+  $.GameScoreSection.setItems(GameScoreRows);
+};
+
+// create listitem row
+// STUDY : http://docs.appcelerator.com/platform/latest/#!/api/Titanium.UI.ListItem
+CTX.createGameSCoreRow = function (GameScoreModel) {
+  var _playerName = GameScoreModel.get('playerName');
+  var _score = GameScoreModel.get('score');
+	var _cheatMode = GameScoreModel.get('cheatMode') ? "TRUE" : "FALSE";
+  return  {
+    template : 'GameScoreTemplate',
+    playerName : { text: _playerName },
+		score : { text: _score },
+		cheatMode : { text: _cheatMode },
+    properties : {
+        itemId : GameScoreModel.id
+    }
+  };
+}
+
 /**
 * scroll end for position save
 */
@@ -45,6 +96,8 @@ CTX.open = function() {
 	//등록
 	CTX.$observer = CTX.$observer || _.extend({}, Backbone.Events);
 	// CTX.$observer.listenTo(CTX.newsCol, 'new:news', redrawAfterRemote);
+
+	CTX.fetchGameScore();
 }
 CTX.close = function() {
 	CTX.$observer.stopListening();
@@ -81,7 +134,7 @@ CTX.handleNavigation = function (e) {
           children: [ $.listView ]
         });
         CTX.ptr.setParent($.mainView);
-        CTX.ptr.on("release", CTX.fetchRecentQuestion);
+        CTX.ptr.on("release", CTX.fetchGameScore);
 
         // restore position
         if (CTX.scrollItemIndex) {
